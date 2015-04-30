0bd56de9bb24bc6f8c11edc8cb179dedf3358701
Beau Gunderson <beau@beaugunderson.com>
2015-04-29 17:41:09 -0700

    log stdin to debug.log; 2.0.0

--- silence-chromium.js
+++ silence-chromium.js
@@ -3,10 +3,23 @@
 'use strict';
 
 var byline = require('byline');
+var fs = require('fs');
 
 require('epipebomb')();
 
 var RE_CHROMIUM_LOG_LINE = /^\[.*?:INFO:(?!CONSOLE)/;
+
+var fileName = process.argv[2] || './debug.log';
+
+try {
+  var debug = fs.createWriteStream(fileName, {flags: 'a'});
+
+  debug.write(new Date().toISOString() + ' silence-chromium starting\n\n');
+
+  process.stdin.pipe(debug);
+} catch (err) {
+  process.stdout.write('could not open "' + fileName + '" for writing\n');
+}
 
 byline(process.stdin).on('data', function (line) {
   if (RE_CHROMIUM_LOG_LINE.test(line)) {
